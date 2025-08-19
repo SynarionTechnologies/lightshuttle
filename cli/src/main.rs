c4ad12dbc1650f0b917bb61b5390cfa93073b90b
Pierrick FONQUERNE <pierrick.fonquerne@gmail.com>
2025-08-19 10:02:14 +0200

    fix: windows build

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,19 +1,7 @@
-
-use lightshuttle_cli::run;
-
-fn main() {
-    match run(users::get_current_uid()) {
-        Ok(_) => println!("LightShuttle CLI is ready."),
-        Err(e) => {
-            eprintln!("{}", e);
-            std::process::exit(1);
-        }
-    }
-
 #[cfg(unix)]
 fn ensure_not_root() {
-    if users::get_current_uid() == 0 {
-        eprintln!("Refusing to run as root.");
+    if let Err(msg) = lightshuttle_cli::run(users::get_current_uid()) {
+        eprintln!("{msg}");
         std::process::exit(1);
     }
 }
@@ -23,6 +11,5 @@ fn ensure_not_root() {}
 
 fn main() {
     ensure_not_root();
-
     println!("LightShuttle CLI is ready.");
 }
--- cli/tests/cli.rs
+++ cli/tests/cli.rs
@@ -1,11 +1,28 @@
-use assert_cmd::prelude::*;
-use predicates::prelude::*;
-use std::process::Command;
+#[cfg(unix)]
+mod unix_cli_tests {
+    use assert_cmd::prelude::*;
+    use predicates::prelude::*;
+    use std::process::Command;
 
-#[test]
-fn fails_when_running_as_root() {
-    let mut cmd = Command::cargo_bin("lightshuttle-cli").unwrap();
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("Refusing to run as root."));
+    #[test]
+    fn fails_when_running_as_root() {
+        let mut cmd = Command::cargo_bin("lightshuttle-cli").unwrap();
+        cmd.assert()
+            .failure()
+            .stderr(predicates::str::contains("Refusing to run as root."));
+    }
+}
+
+#[cfg(windows)]
+mod windows_cli_tests {
+    use assert_cmd::prelude::*;
+    use std::process::Command;
+
+    #[test]
+    fn runs_normally_on_windows() {
+        let mut cmd = Command::cargo_bin("lightshuttle-cli").unwrap();
+        cmd.assert()
+            .success()
+            .stdout(predicates::str::contains("LightShuttle CLI is ready."));
+    }
 }
