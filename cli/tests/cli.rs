3140e09a8507a896d33d405b0ec5c6d535b16f64
Pierrick FONQUERNE <pierrick.fonquerne@gmail.com>
2025-08-19 10:12:40 +0200

    fix: ci build

--- cli/tests/cli.rs
+++ cli/tests/cli.rs
@@ -1,20 +1,30 @@
 #[cfg(unix)]
 mod unix_cli_tests {
     use assert_cmd::prelude::*;
+    use predicates::prelude::*;
     use std::process::Command;
 
     #[test]
-    fn fails_when_running_as_root() {
+    fn ensure_not_root_behavior_matches_current_uid() {
         let mut cmd = Command::cargo_bin("lightshuttle-cli").unwrap();
-        cmd.assert()
-            .failure()
-            .stderr(predicates::str::contains("Refusing to run as root."));
+
+        let is_root = users::get_current_uid() == 0;
+        if is_root {
+            cmd.assert()
+                .failure()
+                .stderr(predicate::str::contains("Refusing to run as root."));
+        } else {
+            cmd.assert()
+                .success()
+                .stdout(predicate::str::contains("LightShuttle CLI is ready."));
+        }
     }
 }
 
 #[cfg(windows)]
 mod windows_cli_tests {
     use assert_cmd::prelude::*;
+    use predicates::prelude::*;
     use std::process::Command;
 
     #[test]
@@ -22,6 +32,6 @@ mod windows_cli_tests {
         let mut cmd = Command::cargo_bin("lightshuttle-cli").unwrap();
         cmd.assert()
             .success()
-            .stdout(predicates::str::contains("LightShuttle CLI is ready."));
+            .stdout(predicate::str::contains("LightShuttle CLI is ready."));
     }
 }
